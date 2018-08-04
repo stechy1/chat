4194c347e09d4eef470e83fb451d931cd49edfa8
petr <stechy12@gmail.com>
2018-08-04 14:29:12 +0200

    Implementace loggeru

--- server/src/main/java/cz/stechy/chat/Server.java
+++ server/src/main/java/cz/stechy/chat/Server.java
@@ -1,8 +1,18 @@
 package cz.stechy.chat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Server {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+
     public static void main(String[] args) throws Exception {
+        LOGGER.trace("Trace log");
+        LOGGER.debug("Debug log");
+        LOGGER.info("Info log");
+        LOGGER.warn("Warn log");
+        LOGGER.error("Error log");
 
     }
 

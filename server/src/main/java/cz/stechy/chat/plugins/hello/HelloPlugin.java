808e8baa3bbcb5a5b78c3c7223adc34c8d91b4e7
petr <stechy12@gmail.com>
2018-09-06 10:51:07 +0200

    Odstranění loggeru ze hello pluginu.

--- server/src/main/java/cz/stechy/chat/plugins/hello/HelloPlugin.java
+++ server/src/main/java/cz/stechy/chat/plugins/hello/HelloPlugin.java
@@ -2,13 +2,8 @@ package cz.stechy.chat.plugins.hello;
 
 import cz.stechy.chat.core.event.IEventBus;
 import cz.stechy.chat.plugins.IPlugin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class HelloPlugin implements IPlugin {
-
-    @SuppressWarnings("unused")
-    private static final Logger LOGGER = LoggerFactory.getLogger(HelloPlugin.class);
 
     public static final String PLUGIN_NAME = "hello";
 
@@ -19,7 +14,7 @@ public class HelloPlugin implements IPlugin {
 
     @Override
     public void init() {
-        LOGGER.info("Inicializace pluginu: {}", getName());
+        System.out.println("Inicializace pluginu: " + getName());
     }
 
     @Override

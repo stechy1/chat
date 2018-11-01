086bf35d87fd4fb95a543aa2517c76488509c886
petr <stechy12@gmail.com>
2018-11-01 13:49:35 +0100

    Listener na zavření okna

--- client/src/main/java/cz/stechy/chat/ChatApp.java
+++ client/src/main/java/cz/stechy/chat/ChatApp.java
@@ -1,5 +1,6 @@
 package cz.stechy.chat;
 
+import cz.stechy.chat.controller.main.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,7 +17,11 @@ public class ChatApp extends Application {
     public void start(Stage stage) throws Exception {
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main/main.fxml"));
         final Parent parent = loader.load();
+        MainController controller = loader.getController();
         final Scene scene = new Scene(parent);
+        stage.setOnCloseRequest(windowEvent -> {
+            controller.onClose();
+        });
         stage.setScene(scene);
         stage.setTitle("Chat - ITnetwork tutorial");
         stage.setWidth(640);
